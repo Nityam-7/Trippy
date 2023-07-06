125f500b158605eca2ae69570f6bb7dcfca79e44
Nityam-7 <118311169+Nityam-7@users.noreply.github.com>
2023-07-06 21:24:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Navbar from "./components/Navbar";
+import { Routes } from "react-router-dom";
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
